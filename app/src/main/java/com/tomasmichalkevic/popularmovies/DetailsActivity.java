a8391ab79c93ea5acc28734586bee3406cee81d2
Tomas Michalkevic <tomas.michalkevic@swedbank.lt>
2018-03-06 23:25:34 +0200

    Fixed the FAB button and started working on the recycler view

--- app/src/main/java/com/tomasmichalkevic/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/tomasmichalkevic/popularmovies/DetailsActivity.java
@@ -5,11 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -22,9 +25,10 @@ import java.util.List;
 
 public class DetailsActivity extends Activity {
 
-//    private List<Trailer> trailerList = new ArrayList<>();
-//    private RecyclerView recyclerView;
-//    private TrailerAdapter trailerAdapter;
+    private List<Trailer> trailerList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private TrailerAdapter trailerAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,10 +40,16 @@ public class DetailsActivity extends Activity {
         TextView releaseTV = findViewById(R.id.release_tv);
         TextView ratingTV = findViewById(R.id.rating_tv);
         TextView descriptionTV = findViewById(R.id.description_tv);
+        FloatingActionButton fab = findViewById(R.id.favourite_fab);
 
-//        recyclerView = findViewById(R.id.trailer_recycler_view);
-//        trailerAdapter = new TrailerAdapter(trailerList);
-//
+        recyclerView = findViewById(R.id.trailer_recycler_view);
+        trailerAdapter = new TrailerAdapter(trailerList);
+
+        recyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+
         Intent intent = getIntent();
         if (intent == null) {
             finish();
@@ -61,16 +71,25 @@ public class DetailsActivity extends Activity {
         ratingTV.setText(String.format("%d/10", Math.round(voteAverage)));
         descriptionTV.setText(overview);
 
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button Clicked - To be implemented", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        recyclerView.setAdapter(trailerAdapter);
+        prepareMockData();
 
     }
 
-//    private void prepareMockData(){
-//        Trailer trailer = new Trailer("592199669251414ab10568ec", "en", "US", "fBNpSRtfIUA", "\\\"Offer He Can't Refuse\\\"", "YouTube", 1080, "Clip");
-//        trailerList.add(trailer);
-//
-//        Trailer trailer1 = new Trailer("592199669251414ab10568ec", "en", "US", "fBNpSRtfIUA", "\\\"Offer He CAN Refuse\\\"", "YouTube", 1080, "Clip");
-//        trailerList.add(trailer1);
-//
-//        trailerAdapter.notifyDataSetChanged();
-//    }
+    private void prepareMockData(){
+        Trailer trailer = new Trailer("592199669251414ab10568ec", "en", "US", "fBNpSRtfIUA", "\\\"Offer He Can't Refuse\\\"", "YouTube", 1080, "Clip");
+        trailerList.add(trailer);
+
+        Trailer trailer1 = new Trailer("592199669251414ab10568ec", "en", "US", "fBNpSRtfIUA", "\\\"Offer He CAN Refuse\\\"", "YouTube", 1080, "Clip");
+        trailerList.add(trailer1);
+
+        trailerAdapter.notifyDataSetChanged();
+    }
 }
